596e99fc56a5418695fcba5d59a2b5cf0bc7c060
mathew-dennis <78794223+mathew-dennis@users.noreply.github.com>
2022-05-15 03:07:42 +0530

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -35,7 +35,7 @@ then
    echo " "
    read re_download
     
-   if [ $re_download==yes ]
+   if [ $re_download = yes ]
    then
       rm -f rootfs.zip droidian_rootfs.zip
       wget https://images.droidian.org/rootfs-api28gsi-all/nightly/arm64/generic/rootfs.zip
@@ -137,7 +137,7 @@ cd $device
 
 #fix me: add dual boot support for a/b device
 
-if [ $dual_boot==yes ]
+if [ $dual_boot = yes ]
 then
    if [ -e vendor.img ]
    then
